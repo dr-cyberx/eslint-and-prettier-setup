3e7d13e9fa167173a7d17641bddfa9a26e6f1cf3
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-12-02 16:47:19 +0530

    project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <Component {...pageProps} />
+);
+export default MyApp;
